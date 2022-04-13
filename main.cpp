4892848a5918dda04322f87e130679b19af8845a
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2022-04-13 13:39:46 +0900

    CG2 01_01

--- main.cpp
+++ main.cpp
@@ -40,16 +40,53 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//自動でサイズを補正する
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-	HWND hwnd= CreateWindow
+	HWND hwnd = CreateWindow
 	(
 		w.lpszClassName,		//クラス名
 		L"DirectXGame",			//タイトルバーの文字
 		WS_OVERLAPPEDWINDOW,	//標準的なウィンドウスタイル
 		CW_USEDEFAULT,			//表示X座標(OSに任せる)
 		CW_USEDEFAULT,			//表示Y座標(OSに任せる)
-		wrc.right-wrc.left,
+		wrc.right - wrc.left,	//ウィンドウ横幅
+		wrc.bottom - wrc.top,	//ウィンドウ縦幅
+		nullptr,				//親ウィンドウハンドル
+		nullptr,				//メニューハンドル
+		w.hInstance,			//呼び出しアプリケーションハンドル
+		nullptr					//オプション
+	);
 
-	)
+	//ウィンドウを表示状態する
+	ShowWindow(hwnd, SW_SHOW);
 
+	MSG msg{};		//メッセージ
+
+	//DirectX初期化処理　ここから
+
+
+	//DirectX初期化処理　ここから
+
+	//ゲームループ
+	while (true)
+	{
+		//メッセージある？
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);	//キー入力メッセージの処理
+			DispatchMessage(&msg);	//プロシージャにメッセージを送る
+		}
+
+		//×ボタンで終了メッセージが来たらゲームループを抜ける
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+
+		//DirecX毎フレーム　ここから
+
+
+		//DirecX毎フレーム　ここまで
+
+	}
+	
 	return 0;
 }
