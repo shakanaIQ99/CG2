8b0d8b3f0c750c093dcc79b9965db7b6af6eb16c
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2022-05-11 13:59:03 +0900

    04_01_完了

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,11 +1,11 @@
-float4 main(float4 pos : POSITION) : SV_POSITION
+#include "Basic.hlsli"
+
+VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD)
 {
-	/*float4 pos2 = pos;
-	const float PI = 3.14159;
-	float angle = PI * 0.25f;
-	pos2.x = pos.x * cos(angle) - pos.y * sin(angle);
-	pos2.y = pos.x * sin(angle) + pos.y * cos(angle);*/
+	VSOutput output; //ピクセルシェーダーnに渡す値
+	output.svpos = pos;
+	output.uv = uv;
 
 
-	return pos;
+	return output;
 }
--- main.cpp
+++ main.cpp
@@ -36,17 +36,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 
 #pragma region 頂点バッファの確保
-	//頂点データ
-	XMFLOAT3 vertices[] =
+
+	//頂点データ構造体
+
+	struct Vertex
 	{
-		{-0.5f,-0.5f,0.0f},
-		{-0.5f,+0.5f,0.0f},
-		{+0.5f,-0.5f,0.0f},
-		{+0.5f,+0.5f,0.0f},
+		XMFLOAT3 pos;	//xyz座標
+		XMFLOAT2 uv;	//uv座標
+	};
+
+
+	//頂点データ
+	Vertex vertices[] =
+	{
+		//x      y     z     u    v
+		{{-0.4f,-0.7f,0.0f},{0.0f,1.0f}},	//左下
+		{{-0.4f,+0.7f,0.0f},{0.0f,0.0f}},	//左上
+		{{+0.4f,-0.7f,0.0f},{1.0f,1.0f}},	//右下
+		{{+0.4f,+0.7f,0.0f},{1.0f,0.0f}},	//右上
 	};
 
 	//頂点データ全体のサイズ=頂点データ一つ分のサイズ*頂点データの要素数
-	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+	UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * _countof(vertices));
 
 	//頂点バッファの設定
 	D3D12_HEAP_PROPERTIES heapProp{};		//ヒープ設定
@@ -78,7 +89,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #pragma region インデックスバッファの設定
 
 	//インデックスデータ
-	uint16_t indices[] =
+	unsigned short indices[] =
 	{
 		0,1,2,//三角形1つ目
 		1,2,3,//三角形2つ目
@@ -113,7 +124,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 #pragma region 頂点バッファへのデータ転送
 	//GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
-	XMFLOAT3* vertMap = nullptr;
+	Vertex* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	assert(SUCCEEDED(result));
 	//全頂点に対して
@@ -151,7 +162,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//頂点バッファのサイズ
 	vbView.SizeInBytes = sizeVB;
 	//頂点1つ分のデータサイズ
-	vbView.StrideInBytes = sizeof(XMFLOAT3);
+	vbView.StrideInBytes = sizeof(vertices[0]);
 
 #pragma endregion 頂点バッファビュー
 
@@ -233,7 +244,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 
 	//頂点レイアウト
-	D3D12_INPUT_ELEMENT_DESC inputLayout[] = { {"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,D3D12_APPEND_ALIGNED_ELEMENT,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0}, };
+	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+		{/*xyZ座標*/"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,D3D12_APPEND_ALIGNED_ELEMENT,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
+		{/*座標uv*/"TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT,0,D3D12_APPEND_ALIGNED_ELEMENT,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
+	};
 
 
 #pragma region グラフィックスパイプライン
