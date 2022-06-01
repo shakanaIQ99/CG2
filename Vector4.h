2fbcee4efaf0ec0a0c77b37abe18237905c9bd07
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2022-06-01 13:11:31 +0900

    Vector2,Vector3,Vector4クラス追加

---
+++ Vector4.h
@@ -0,0 +1,14 @@
+#pragma once
+class Vector4
+{
+public:
+	float x;
+	float y;
+	float z;
+	float w;
+
+public:
+	Vector4();                                  
+	Vector4(float x, float y, float z, float w);
+};
+
